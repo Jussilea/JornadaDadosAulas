6ca8d8d58be68e0a9db758abb84fd848a5331ebb
Jussilea <jussileagf@gmail.com>
2025-12-22 22:00:22 -0300

    Conclusão da Aula01

--- Aula01.py
+++ Aula01.py
@@ -1,1 +1,25 @@
+# Aplicando print
 print("Teste!")
+
+print("Teste de print primeira aula")
+
+print (3+7)
+
+print ("Oi" + " " + "pessoal")
+
+#Aplicando input
+input ("Digite o seu nome:")
+
+print(input ("Digite o seu nome:"))
+
+print("Oi," + input("Digite seu nome:") + ":)") 
+
+numero = 3 # int
+numero_decimal = 4.78 # float
+nome_de_usuario = Jussilea # string
+verdadeiro = True # bool
+print(type(numero)) # type mostra o tipo da variável
+
+# os nomes de variáveis são case-sensitive
+# variáveis sempre minusculas, separadas com _ / _ a esquerda da variável, esta é secreta
+# constantes sempre maisculas
