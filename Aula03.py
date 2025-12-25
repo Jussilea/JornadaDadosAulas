7a41282b7709ceec9407ab9310b00b4a6ba08696
Jussilea <jussileagf@gmail.com>
2025-12-25 20:26:53 -0300

    Aula 03

---
+++ Aula03.py
@@ -0,0 +1,53 @@
+# Debug, run and debug
+'''print("Esse é o primeiro comando.")
+x = 5
+print("Esse é o primeiro comando.")
+x = 6
+print("Esse é o primeiro comando.")
+print(x)'''
+
+#Controle de fluxo
+'''if avaliar/checar uma variável, verificar as condições, avaliações em linha / a ordem dos fatores é muito importante
+x = 2
+if x < 0:
+    x = 0
+    print("Negative changed to zero")
+elif x == 0:
+    print("Zero")
+elif == 1:
+    print("Single")
+else:
+    print("More")
+
+for
+no range, ele começa pelo primeiro, mas não chega no último, não printa 
+for i range (2,6):
+    print(i)
+
+lista_alunos = ["rafael", "fabio", "luciano"]
+for aluno in lista_alunos
+    print(aluno)
+
+[] - lista
+{} - dicionário '''
+# Exemplo dicionário
+'''brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'Sao Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil)
+
+
+estado = dict()
+brasil = list()
+for c in range(0.3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+for e in brasil:
+    for v in e.values():
+        print(v, end=' ')
+    print()
+
+'''
